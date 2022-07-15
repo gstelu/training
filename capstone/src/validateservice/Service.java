c8d7d047bbd134940b23a1cb2e288aa0dbc719b3
gstelu <108110501+gstelu@users.noreply.github.com>
2022-07-15 09:06:12 +0530

    Delete capstone/src directory

--- capstone/src/validateservice/Service.java
+++
@@ -1,5 +0,0 @@
-package validateservice;
-
-public class Service {
-
-}
--- capstone/src/validationmain/Main.java
+++
@@ -1,9 +0,0 @@
-package validationmain;
-
-import java.io.File;
-
-public class Main {
-public static void main(String[] args) {
-	File f= new File("C:\\Users\\gstelu\\Desktop\\project.txt");
-}
-}
