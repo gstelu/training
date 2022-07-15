3f25f90ea10f9771dbb1f79b9c58265af62a79b1
gstelu <108110501+gstelu@users.noreply.github.com>
2022-07-15 09:00:26 +0530

    Add files via upload

---
+++ capstone/src/validateservice/Service.java
@@ -0,0 +1,5 @@
+package validateservice;
+
+public class Service {
+
+}
---
+++ capstone/src/validationmain/Main.java
@@ -0,0 +1,9 @@
+package validationmain;
+
+import java.io.File;
+
+public class Main {
+public static void main(String[] args) {
+	File f= new File("C:\\Users\\gstelu\\Desktop\\project.txt");
+}
+}
